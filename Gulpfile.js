cbdd1ed64465a7701efb9a6c401091fafe3bc579
Wouter Kooyman van Guldener <wouter@wick-it.nl>
2015-01-12 11:28:31 +0100

    Added lib/.components to gulp watchfile.

--- Gulpfile.js
+++ Gulpfile.js
@@ -160,6 +160,7 @@ gulp.task('serve', ['styles', 'lib'], function () {
   });
 
   gulp.watch(['app/**/*.html'], reload);
+  gulp.watch(['lib/.components/**/*.html'], reload);
   gulp.watch(['app/styles/**/*.{scss,css}'], ['styles', reload]);
   gulp.watch(['app/scripts/**/*.js'], ['jshint']);
   gulp.watch(['app/images/**/*'], reload);
