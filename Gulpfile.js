a62bad4ba4e5dc8e329cfee5b91c380d3aac5371
svenpopping <s.popping@student.tudelft.nl>
2015-04-30 23:03:39 +0200

    Merge branch 'master' of https://github.com/WISVCH/LANcie into refactor

--- Gulpfile.js
+++ Gulpfile.js
@@ -132,13 +132,13 @@ gulp.task('serve', ['sass:app:serve', 'sass:lib:serve', 'lib:serve', 'json:serve
 gulp.task('build', ['clean'], function (cb) {
   // runSequence('styles', ['jshint', 'html', 'lib', 'images', 'fonts', 'copy', 'json', 'coffee'], cb);
   runSequence(
-    'clean',
+    // 'clean',
     'coffee:build', 
-    'coffee:min', 
+    // 'coffee:min', 
     ['sass:app:build', 'sass:lib:build'], 
     'html',
     'lib:build',
-    'images:build',
+    // 'images:build',
     'json:min',
     cb);
 });
